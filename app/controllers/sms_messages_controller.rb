be9f4e09ed0743e938cb2d054530b11c2b2a7205
Caio Ramos <kaineo@hotmail.com>
2021-06-03 20:47:38 -0300

    refactor

--- app/controllers/sms_messages_controller.rb
+++ app/controllers/sms_messages_controller.rb
@@ -1,27 +1,23 @@
-
 require 'twilio-ruby'
 require './environment'
 
 class SmsMessagesController < ApplicationController
+  def create
+    # ACCOUNT_SID and AUTH_TOKEN below are imported from environment file on line 2
+    client = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN) 
 
-    def create
-        #ACCOUNT_SID and AUTH_TOKEN below are imported from environment file on line 2
-        client = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN) 
+    client.messages.create(
+    # TWILIO_NUMBER below is imported from environment file on line 2
+    from: TWILIO_NUMBER,
+    to: sms_message_params['mobile_number'],
+    body: sms_message_params['message']
+    )
+    render json: client
+  end
 
-        client.messages.create(
-        #TWILIO_NUMBER below is imported from environment file on line 2
-        from: TWILIO_NUMBER,
-        to: sms_message_params['mobile_number'],
-        body: sms_message_params['message']
-        )
-    
-        render json: client
-    end
+  private
 
-    private
-
-    def sms_message_params
-        params.require(:sms_message).permit(:mobile_number, :message)
-    end
-
+  def sms_message_params
+    params.require(:sms_message).permit(:mobile_number, :message)
+  end
 end
